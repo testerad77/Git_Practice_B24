42c13e51bbc70c45ab763983a9f39b55c1b76a3b
adilyakhissayeva <adilya.usa@gmail.com>
2021-10-03 16:29:19 -0400

    added wooden spoon

---
+++ src/tests/WoodenSpoonTest.java
@@ -0,0 +1,15 @@
+package tests;
+
+public class WoodenSpoonTest {
+
+    public static void main(String[] args) {
+
+        for(int i = 0; i < 5; i++){
+
+            System.out.println("Wooden Spoon");
+
+        }
+
+    }
+
+}
