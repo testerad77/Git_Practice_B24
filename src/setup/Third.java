4b1dea4667456e55cc522e9ae210d40213d5bd6a
adilyakhissayeva <adilya.usa@gmail.com>
2021-10-03 12:46:01 -0400

    We create third file

---
+++ src/setup/Third.java
@@ -0,0 +1,13 @@
+package setup;
+
+
+public class Third {
+    public static void main(String[] args) {
+        System.out.println("This is our third file");
+
+        // adding more lines
+
+        System.out.println("I like Java");
+    }
+
+}
